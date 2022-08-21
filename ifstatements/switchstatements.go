03a79ae3f8d6a39160d30d2461411fd73b002e9b
marvin espira <espiramarvin@gmail.com>
2022-08-21 16:07:58 +0300

    adds switch statements updates

---
+++ ifstatements/switchstatements.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	switch 3 {
+	case 1, 5, 10: // falling through/ testings multiple cases
+		fmt.Println("one, five or ten")
+	case 2, 6, 7:
+		fmt.Println("two, six or seven")
+	default:
+		fmt.Println("not one or two")
+	}
+
+	// another example with tags eg. i
+	switch i := 2 + 3; i {
+	case 1, 5, 10: // falling through/ testings multiple cases
+		fmt.Println("one, five or ten")
+	case 2, 6, 7:
+		fmt.Println("two, six or seven")
+	default:
+		fmt.Println("not one or two")
+	}
+
+	// another example with tagless syntax
+	a := 10
+	switch {
+	case a <= 10:
+		fmt.Println("less than or equal to ten")
+		// fallthrough
+	case a <= 20:
+		fmt.Println("less than or equal to twenty")
+	default:
+		fmt.Println("greater than twenty")
+	}
+
+	// type switch
+	var i interface{} = 1 // integer
+	// var i interface{} = 1.58   // float64
+	// var i interface{} = "wick" // string
+	// var i interface{} = [3]int{}   // array of int with length 3
+	switch i.(type) {
+	case int:
+		fmt.Println("i is an int")
+		break
+		fmt.Println("This will not print")
+	case float64:
+		fmt.Println("i is a float64")
+	case string:
+		fmt.Println("i is a string")
+	case [3]int:
+		fmt.Println("i is an array of int with len 3")
+	default:
+		fmt.Println("i is another type")
+
+	}
+
+}
