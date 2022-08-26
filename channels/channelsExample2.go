1636f5ef736b7b6f77a6eaf70fededb1e0c0af8f
marvin espira <espiramarvin@gmail.com>
2022-08-26 08:39:39 +0300

    addds updates

---
+++ channels/channelsExample2.go
@@ -0,0 +1,27 @@
+// - channels can have multiple senders and receivers
+
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var wg = sync.WaitGroup{}
+
+func main() {
+	ch := make(chan int)
+	wg.Add(2)
+	go func() {
+		i := <-ch
+		fmt.Println(i)
+		ch <- 27
+		wg.Done()
+	}()
+	go func() {
+		ch <- 42
+		fmt.Println(<-ch)
+		wg.Done()
+	}()
+	wg.Wait()
+}
